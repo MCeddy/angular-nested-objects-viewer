8b7339cee275f72fc9d58da8c7ce64e4cc33650c
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2020-10-14 23:00:29 +0200

    import missing modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 import { AppComponent } from './app.component';
+import { FieldComponent } from './field/field.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, FieldComponent],
+    imports: [BrowserModule, FormsModule, CommonModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
